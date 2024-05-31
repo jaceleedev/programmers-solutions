083fa23f4913f9c8649af3322bec54b5dda164c8
jaceleedev <jaceleedev@gmail.com>
2024-05-31 20:42:51 +0900

    feat: :technologist: solve 문자열 붙여서 출력하기

---
+++ lv.0/문자열 붙여서 출력하기.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = line.split(' ');
+}).on('close', function () {
+  const [str1, str2] = input.map(String);
+  console.log(`${str1}${str2}`);
+});
