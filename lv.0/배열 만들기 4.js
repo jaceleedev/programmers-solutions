43877e46d6fba66b88600f9b02556097c684da47
jaceleedev <jaceleedev@gmail.com>
2024-07-11 16:54:13 +0900

    feat: :technologist: solve 배열 만들기 4

---
+++ lv.0/배열 만들기 4.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+  const stk = [];
+  let i = 0;
+
+  while (i < arr.length) {
+    if (stk.length === 0 || stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i]);
+      ++i;
+    } else if (stk[stk.length - 1] >= arr[i]) {
+      stk.pop();
+    }
+  }
+
+  return stk;
+}
